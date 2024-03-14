a3594a454a61815ab658e466c257d0fc9a862d93
João Pedro Pereira <joao_pa_pereira@estudante.sc.senai.br>
2024-03-14 19:27:25 -0300

    up

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require ("express");
+const userRouter = require ("./router/userRouter.js");
 
 const PORT = 3000;
 
@@ -8,6 +9,8 @@ app.get ("/", (req, res) => {
     res.send('<h1> Hello World!</h1');
 
 });
+
+app.use(';api', )
 
 app.listen (PORT, () => {
     console.log ("Servidor online");
