056e2d01bf7dc322494895d3d4020308b3434663
João Pedro Pereira <joao_pa_pereira@estudante.sc.senai.br>
2024-03-14 19:19:26 -0300

    up4

---
+++ src/controller/userController.js
@@ -0,0 +1,19 @@
+const userService = require('../service/user.js');
+
+async function getAllUser(req, res){
+    try {
+        const rows = await userService.getAllUser();
+        res.status(200).json(rows);
+
+    }catch(error){
+        res.status(500).send({
+            message: "Error getting users",
+            body: error.message,
+        });
+
+    }
+}
+
+module.exports = {
+    getAllUser,
+};
